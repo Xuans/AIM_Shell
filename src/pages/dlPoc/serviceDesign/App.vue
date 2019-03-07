06db767e7352af39a3b78d65d36d188b1324a14e
yanjiaxun <yanjiaxun@agree.com.cn>
2019-03-07 09:03:59 +0800

    服务编排

---
+++ src/pages/dlPoc/serviceDesign/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <workbench :model="model">
+
+    <el-button-group slot="centerTool">
+      <el-button icon="el-icon-delete" :disabled="!target.lastest" @click="editorHandle('delete')"></el-button>
+      <el-button icon="el-icon-edit-outline" :disabled="!target.lastest" @click="editorHandle('save')"></el-button>
+      <el-button icon="el-icon-upload2" :disabled="!target.lastest" @click="editorHandle('upload')"></el-button>
+    </el-button-group>
+
+    <el-select slot="centerTool"
+               v-model="target.head"
+               placeholder="待发布"
+    >
+      <el-option
+              v-for="item in target.versions"
+              :key="item.name"
+              :label="`v${item.name}.0`"
+              :value="item.name">
+      </el-option>
+    </el-select>
+
+    <shell-flow ref="editor" slot="mainPage" :target="target">
+      <template slot="form" slot-scope="{store}">
+        <shell-flow-panel :store="store"></shell-flow-panel>
+      </template>
+    </shell-flow>
+  </workbench>
+</template>
+
+
+<script>
+import Vue from 'vue'
+import ShellFlow from '../../../editor/shell-flow.vue'
+import ShellFlowPanel from '../../../components/Panel/ShellFlowPanel.vue'
+import 'element-ui/lib/theme-chalk/index.css'
+import '../../../assets/flow-style.css'
+import '../../../assets/flow1/iconfont.css'
+import ElementUi from 'element-ui'
+import ExternalApi from '../../../plugin/externalApi'
+
+import workbench from '../../../components/workbench.vue'
+
+Vue.config.productionTip = false
+Vue.use(ElementUi)
+Vue.use(ExternalApi)
+
+export default {
+  name: 'app',
+  props: ['target'],
+  data: function () {
+    return {
+      model: {
+        name:'服务编排',
+        paths:[{name:'path'},{to:'to'},{me:'me'}]
+      }
+    }
+  },
+
+  methods: {
+    editorHandle (fnName) {
+      this.$refs.editor.store[fnName]()
+    }
+  },
+
+  components: {
+    ShellFlow,
+    ShellFlowPanel,
+    workbench
+  }
+}
+</script>
+
+<style>
+.aim-shell-editor{
+  position:absolute;
+  top:0;
+  left:0;
+  right:0;
+  bottom: 15px;
+  overflow: hidden;
+  border-radius: 6px;
+  border:1px solid #efefef;
+}
+.aim-shell-editor section{
+  border: none;
+  margin:0;
+}
+</style>
---
+++ src/pages/dlPoc/serviceDesign/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+class Target {
+  constructor ({serviceId} = {serviceId: 0}) {
+    this.inputId = this.id = serviceId
+    this.type = 0
+    this.versions = Array.of({name: 0}, {name: 1}, {name: 2})
+    this.head = 0
+  }
+
+  get lastest () {
+    return this.head === this.versions[this.versions.length - 1].name
+  }
+}
+
+(function () {
+  window.Vue = Vue
+
+  const app = window.app
+  const AppCtr = Vue.extend(App)
+
+  if (app && app.dispatcher) {
+    app.dlPoc = app.dlPoc || {}
+
+    let isFirst = true
+    const dispay = function (viewId, serviceId) {
+      let ins = new AppCtr({
+        propsData: {
+          target: new Target({serviceId})
+        }
+      }).$mount(`#${viewId}`)
+
+      window.AIM_SHELL = ins
+
+      return ins
+    }
+
+    app.dlPoc.editor = function (viewId, serviceId) {
+      return dispay(viewId, serviceId)
+    }
+  } else {
+    window.AIM_SHELL = new AppCtr({propsData: { target: new Target() }}).$mount('#app')
+  }
+}())
