1dea9474012a550c922cdf822dfebe0d13b36aac
anrainie <anrainie@agree.com.cn>
2019-03-12 14:53:49 +0800

    merge

---
+++ src/components/Panel/MutilPanel_BASE_9416.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-tabs type="border-card">
+        <el-tab-pane label="服务参数">
+            <service-params></service-params>
+        </el-tab-pane>
+        <el-tab-pane label="日志">
+
+        </el-tab-pane>
+        <el-tab-pane label="任务列表">
+
+        </el-tab-pane>
+      </el-tabs>
+</template>
+
+<script>
+  import ServiceParams from './ServiceParams.vue'
+
+  export default {
+    name: 'MutilPanel',
+    props: { store: Object },
+    provide () {
+      return {
+        store: this.store
+      }
+    },
+    components: {
+      ServiceParams
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
